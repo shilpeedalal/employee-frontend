df7339683bd1422e7726cc0073975f5b90f96421
Shilpee <shilpeedalal937@gmail.com>
2023-09-27 00:00:41 +0530

    changes in helper.js

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3000"
-// export const BASE_URL = "https://mernappbackend.onrender.com"
+// export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://employee-backend-tjun.onrender.com"
